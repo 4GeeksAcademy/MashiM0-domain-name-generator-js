c3c7816179a22c4c0436ba5f8039596ee7ae4577
MashiM0 <massimog.boschettib@gmail.com>
2025-02-05 23:06:33 -0400

    Updated script.js

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-function wordSeeker(str){
-    return str[Math.floor(Math.random() * str.length)]
+function wordSeeker(arr){
+    return arr[Math.floor(Math.random() * arr.length)]
 }
 document.getElementById("excuse").innerHTML = wordSeeker(who) + " " + wordSeeker(action)+ " " + wordSeeker(what) + " " + wordSeeker(when);
