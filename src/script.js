908bd30ef84cd99629af61905d59c0f9acc44169
MashiM0 <massimog.boschettib@gmail.com>
2025-02-06 02:47:12 +0000

    added functions

--- src/script.js
+++ src/script.js
@@ -3,4 +3,7 @@ let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-document.getElementById("excuse").innerHTML = who[Math.floor(Math.random() * who.length)] + " " + action[Math.floor(Math.random() * action.length)]+ " " + what[Math.floor(Math.random() * what.length)]+ " " + when[Math.floor(Math.random() * when.length)];
+function wordSeeker(str){
+    return str[Math.floor(Math.random() * str.length)]
+}
+document.getElementById("excuse").innerHTML = wordSeeker(who) + " " + wordSeeker(action)+ " " + wordSeeker(what) + " " + wordSeeker(when);
