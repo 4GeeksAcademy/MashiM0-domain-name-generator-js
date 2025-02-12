9c82ee797c9b857cb558820a2eb9cb33d0b25a42
MashiM0 <massimog.boschettib@gmail.com>
2025-02-12 23:51:28 +0000

    refactor script.js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,33 @@
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+const who = [
+    'The dog',
+    'My grandma',
+    'The mailman',
+    'My bird'
+];
+
+const action = [
+    'ate',
+    'peed',
+    'crushed',
+    'broke'
+];
+
+const what = [
+    'my homework',
+    'my phone',
+    'the car'
+];
+
+const when = [
+    'before the class',
+    'when I was sleeping',
+    'while I was exercising',
+    'during my lunch',
+    'while I was praying'
+];
 
 function wordSeeker(arr){
     return arr[Math.floor(Math.random() * arr.length)]
 }
-document.getElementById("excuse").innerHTML = wordSeeker(who) + " " + wordSeeker(action)+ " " + wordSeeker(what) + " " + wordSeeker(when);
+
+document.getElementById('excuse').innerHTML = wordSeeker(who) + ' ' + wordSeeker(action)+ ' ' + wordSeeker(what) + ' ' + wordSeeker(when);
